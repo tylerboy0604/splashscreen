e446ef191abecf923e66d452064a3ee15dc87e82
tylerboy0604 <74601919+tylerboy0604@users.noreply.github.com>
2023-10-22 12:56:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        var iv:ImageView = findViewById(R.id.ImageView5G)
+
+        iv.alpha = 0f
+
+        iv.animate().setDuration(5000).alpha(1f).withEndAction(){
+            val i = Intent(this, LoginActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+    }
+}
